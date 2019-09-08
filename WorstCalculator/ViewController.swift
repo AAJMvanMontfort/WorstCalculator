337878d1a41a4609076a4a94b7262ddc2b6dc43d
AAJMvanMontfort <lout@me.com>
2019-09-08 17:15:19 +0200

    Initial Commit

---
+++ WorstCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorstCalculator
+//
+//  Created by AAJM van Montfort on 08/09/2019.
+//  Copyright © 2019 AAJM van Montfort. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
